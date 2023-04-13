8b6527f1f1791a7bc9f6257ae4bb4a59e9007bfe
Wohlstand <admin@wohlnet.ru>
2023-04-13 12:35:48 +0300

    Updated libADLMIDI. Again. Added mssing files

--- ADLMIDI-Player/src/main/cpp/update.sh
+++ ADLMIDI-Player/src/main/cpp/update.sh
@@ -3,10 +3,11 @@
 rm -Rfv include
 rm -Rfv src
 rm -v CMakeLists.txt
+rm -v libADLMIDIConfig.cmake.in
 
 cp -av /home/vitaly/_git_repos/libADLMIDI/include .
 cp -av /home/vitaly/_git_repos/libADLMIDI/src .
 cp -v /home/vitaly/_git_repos/libADLMIDI/CMakeLists.txt .
+cp -v /home/vitaly/_git_repos/libADLMIDI/libADLMIDIConfig.cmake.in .
 
 read -n 1
-
